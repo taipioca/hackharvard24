cf6b9c88b420eddea23d25c77cfa2380862aaee5
josiexw <jxwang4053@gmail.com>
2024-10-12 18:42:00 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -443,9 +443,9 @@ const GlobeDemo = () => {
             </h2>
           </motion.div>
           <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none  z-40" />
-          {/* <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
+          <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
           <World data={sampleArcs} globeConfig={globeConfig} />
-        </div> */}
+        </div>
         </div>
       </div>
     </div>
