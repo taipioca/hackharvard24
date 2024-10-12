3398fc67c61c4af1a512bcefdbed1e62bc8e3468
unknown <jxwang4053@gmail.com>
2024-10-12 13:10:02 -0400

    migration lines

--- app/(default)/predict/page.tsx
+++ app/(default)/predict/page.tsx
@@ -28,7 +28,6 @@ const cityPositions: { [key: string]: [number, number, number] } = {
   "Baltimore, MD": [2.5, 0.7, 0],
   "Barnstable Town, MA": [2.7, 1.6, 0],
   "Baton Rouge, LA": [0.5, -1.4, 0],
-  "Bend, OR": [-2.4, 1.4, 0],
   "Birmingham, AL": [1.2, -1.0, 0],
   "Boise City, ID": [-2.1, 1.3, 0],
   "Boston, MA": [2.7, 1.5, 0],
@@ -47,14 +46,11 @@ const cityPositions: { [key: string]: [number, number, number] } = {
   "Clarksville, TN": [1.1, -0.5, 0],
   "Cleveland, OH": [1.2, 0.7, 0],
   "Colorado Springs, CO": [-1.1, 0.6, 0],
-  "Columbia, MO": [0.4, 0.2, 0],
   "Columbia, SC": [1.7, -1.1, 0],
   "Columbus, OH": [1.3, 0.4, 0],
-  "Concord, NH": [2.6, 1.6, 0],
   "Corpus Christi, TX": [-0.3, -1.2, 0],
   "Crestview, FL": [2.0, -1.3, 0],
   "Dallas, TX": [-0.2, -0.5, 0],
-  "Daphne, AL": [1.2, -1.3, 0],
   "Davenport, IA": [0.4, 0.6, 0],
   "Dayton, OH": [1.2, 0.5, 0],
   "Deltona, FL": [2.1, -1.4, 0],
@@ -77,22 +73,17 @@ const cityPositions: { [key: string]: [number, number, number] } = {
   "Harrisburg, PA": [2.4, 1.0, 0],
   "Hartford, CT": [2.6, 1.3, 0],
   "Hickory, NC": [1.7, -0.8, 0],
-  "Hilton Head Island, SC": [1.7, -1.3, 0],
   "Houston, TX": [0, -0.8, 0],
-  "Huntsville, AL": [1.3, -1.0, 0],
   "Indianapolis, IN": [0.8, 0.7, 0],
   "Jacksonville, FL": [2.0, -1.2, 0],
-  "Jacksonville, NC": [1.9, -1.0, 0],
   "Kansas City, MO": [0.3, 0.1, 0],
   "Killeen, TX": [-0.3, -0.9, 0],
   "Knoxville, TN": [1.4, -0.6, 0],
-  "Lake Havasu City, AZ": [-1.7, -0.1, 0],
   "Lakeland, FL": [2.1, -1.3, 0],
   "Lancaster, PA": [2.4, 1.1, 0],
   "Lansing, MI": [1.1, 1.0, 0],
   "Las Vegas, NV": [-2.0, 0.3, 0],
   "Lexington, KY": [1.2, 0.1, 0],
-  "Lincoln, NE": [0.6, 0.5, 0],
   "Little Rock, AR": [0.3, -0.6, 0],
   "Los Angeles, CA": [-2.5, 0.2, 0],
   "Louisville, KY": [1.1, 0, 0],
@@ -107,7 +98,6 @@ const cityPositions: { [key: string]: [number, number, number] } = {
   "Naples, FL": [2.2, -1.5, 0],
   "Nashville, TN": [1.1, -0.5, 0],
   "New Haven, CT": [2.6, 1.4, 0],
-  "New Orleans, LA": [0.5, -1.5, 0],
   "New York, NY": [2.5, 1.2, 0],
   "North Port, FL": [2.1, -1.5, 0],
   "Ogden, UT": [-1.9, 0.9, 0],
@@ -116,7 +106,6 @@ const cityPositions: { [key: string]: [number, number, number] } = {
   "Orlando, FL": [2.0, -1.3, 0],
   "Oxnard, CA": [-2.4, 0.2, 0],
   "Palm Bay, FL": [2.1, -1.4, 0],
-  "Panama City, FL": [1.8, -1.3, 0],
   "Pensacola, FL": [1.5, -1.2, 0],
   "Peoria, IL": [0.5, 0.7, 0],
   "Philadelphia, PA": [2.4, 1.1, 0],
@@ -125,7 +114,6 @@ const cityPositions: { [key: string]: [number, number, number] } = {
   "Port St. Lucie, FL": [2.1, -1.5, 0],
   "Portland, ME": [2.7, 1.7, 0],
   "Portland, OR": [-2.3, 1.7, 0],
-  "Poughkeepsie, NY": [2.5, 1.3, 0],
   "Prescott Valley, AZ": [-1.7, 0.2, 0],
   "Providence, RI": [2.7, 1.6, 0],
   "Provo, UT": [-1.8, 0.8, 0],
@@ -135,7 +123,6 @@ const cityPositions: { [key: string]: [number, number, number] } = {
   "Reno, NV": [-2.1, 0.6, 0],
   "Richmond, VA": [2.1, 0.8, 0],
   "Riverside, CA": [-2.4, 0.1, 0],
-  "Rochester, MN": [0.3, 1.3, 0],
   "Rochester, NY": [2.4, 1.2, 0],
   "Rockford, IL": [0.6, 1.0, 0],
   "Sacramento, CA": [-2.4, 0.8, 0],
@@ -153,7 +140,6 @@ const cityPositions: { [key: string]: [number, number, number] } = {
   "Seattle, WA": [-2.3, 1.8, 0],
   "Sebastian, FL": [2.1, -1.5, 0],
   "Spokane, WA": [-2.2, 1.7, 0],
-  "Springfield, IL": [0.6, 0.6, 0],
   "Springfield, MA": [2.6, 1.5, 0],
   "Springfield, MO": [0.3, 0.3, 0],
   "St. Louis, MO": [0.3, 0.5, 0],
@@ -168,15 +154,232 @@ const cityPositions: { [key: string]: [number, number, number] } = {
   "Virginia Beach, VA": [2.2, 0.7, 0],
   "Visalia, CA": [-2.4, 0.2, 0],
   "Washington, DC": [2.3, 0.8, 0],
-  "Wilmington, NC": [1.9, -1.0, 0],
   "Winston, NC": [1.7, -0.8, 0],
   "Worcester, MA": [2.6, 1.4, 0],
   "York, PA": [2.4, 1.0, 0],
   "Youngstown, OH": [1.3, 0.7, 0]
 };
 
-const citiesToLabel = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX", "Phoenix, AZ"]
+const citiesToLabel = [
+  "New York, NY",
+  "Los Angeles, CA",
+  "Chicago, IL",
+  "Houston, TX",
+  "Phoenix, AZ",
+  "Urban Honolulu, HI",
+  "San Francisco, CA",
+  "Philadelphia, PA",
+  "Boston, MA",
+  "Baltimore, MD",
+  "Miami, FL"
+]
 
+const migrations = [
+  { from: "Akron, OH", to: "Cleveland, OH" },
+  { from: "Albany, NY", to: "New York, NY" },
+  { from: "Albuquerque, NM", to: "Phoenix, AZ" },
+  { from: "Allentown, PA", to: "Philadelphia, PA" },
+  { from: "Asheville, NC", to: "Charlotte, NC" },
+  { from: "Atlanta, GA", to: "Miami, FL" },
+  { from: "Augusta, GA", to: "Atlanta, GA" },
+  { from: "Austin, TX", to: "Dallas, TX" },
+  { from: "Bakersfield, CA", to: "Los Angeles, CA" },
+  { from: "Baltimore, MD", to: "Washington, DC" },
+  { from: "Barnstable Town, MA", to: "Boston, MA" },
+  { from: "Baton Rouge, LA", to: "New Orleans, LA" },
+  { from: "Bend, OR", to: "Portland, OR" },
+  { from: "Birmingham, AL", to: "Atlanta, GA" },
+  { from: "Boise City, ID", to: "Salt Lake City, UT" },
+  { from: "Boston, MA", to: "New York, NY" },
+  { from: "Boulder, CO", to: "Denver, CO" },
+  { from: "Bremerton, WA", to: "Seattle, WA" },
+  { from: "Bridgeport, CT", to: "New York, NY" },
+  { from: "Buffalo, NY", to: "Rochester, NY" },
+  { from: "Canton, OH", to: "Cleveland, OH" },
+  { from: "Cape Coral, FL", to: "Miami, FL" },
+  { from: "Cedar Rapids, IA", to: "Des Moines, IA" },
+  { from: "Charleston, SC", to: "Savannah, GA" },
+  { from: "Charlotte, NC", to: "Atlanta, GA" },
+  { from: "Chattanooga, TN", to: "Nashville, TN" },
+  { from: "Chicago, IL", to: "Los Angeles, CA" },
+  { from: "Cincinnati, OH", to: "Dayton, OH" },
+  { from: "Clarksville, TN", to: "Nashville, TN" },
+  { from: "Cleveland, OH", to: "Columbus, OH" },
+  { from: "Colorado Springs, CO", to: "Denver, CO" },
+  { from: "Columbia, MO", to: "Kansas City, MO" },
+  { from: "Columbia, SC", to: "Charlotte, NC" },
+  { from: "Columbus, OH", to: "Cincinnati, OH" },
+  { from: "Concord, NH", to: "Boston, MA" },
+  { from: "Corpus Christi, TX", to: "Houston, TX" },
+  { from: "Crestview, FL", to: "Pensacola, FL" },
+  { from: "Dallas, TX", to: "Austin, TX" },
+  { from: "Daphne, AL", to: "Mobile, AL" },
+  { from: "Davenport, IA", to: "Des Moines, IA" },
+  { from: "Dayton, OH", to: "Cincinnati, OH" },
+  { from: "Deltona, FL", to: "Orlando, FL" },
+  { from: "Denver, CO", to: "Boulder, CO" },
+  { from: "Des Moines, IA", to: "Omaha, NE" },
+  { from: "Detroit, MI", to: "Grand Rapids, MI" },
+  { from: "Duluth, MN", to: "Minneapolis, MN" },
+  { from: "Durham, NC", to: "Raleigh, NC" },
+  { from: "El Paso, TX", to: "Austin, TX" },
+  { from: "Eugene, OR", to: "Portland, OR" },
+  { from: "Fayetteville, AR", to: "Little Rock, AR" },
+  { from: "Fayetteville, NC", to: "Raleigh, NC" },
+  { from: "Flint, MI", to: "Detroit, MI" },
+  { from: "Fort Collins, CO", to: "Denver, CO" },
+  { from: "Fresno, CA", to: "Los Angeles, CA" },
+  { from: "Grand Rapids, MI", to: "Detroit, MI" },
+  { from: "Greeley, CO", to: "Fort Collins, CO" },
+  { from: "Greensboro, NC", to: "Charlotte, NC" },
+  { from: "Greenville, SC", to: "Charleston, SC" },
+  { from: "Harrisburg, PA", to: "Philadelphia, PA" },
+  { from: "Hartford, CT", to: "New Haven, CT" },
+  { from: "Hickory, NC", to: "Charlotte, NC" },
+  { from: "Hilton Head Island, SC", to: "Savannah, GA" },
+  { from: "Houston, TX", to: "Austin, TX" },
+  { from: "Huntsville, AL", to: "Birmingham, AL" },
+  { from: "Indianapolis, IN", to: "Chicago, IL" },
+  { from: "Jacksonville, FL", to: "Orlando, FL" },
+  { from: "Jacksonville, NC", to: "Camp Lejeune, NC" },
+  { from: "Kansas City, MO", to: "St. Louis, MO" },
+  { from: "Killeen, TX", to: "Austin, TX" },
+  { from: "Knoxville, TN", to: "Nashville, TN" },
+  { from: "Lake Havasu City, AZ", to: "Las Vegas, NV" },
+  { from: "Lakeland, FL", to: "Tampa, FL" },
+  { from: "Lancaster, PA", to: "Philadelphia, PA" },
+  { from: "Lansing, MI", to: "Grand Rapids, MI" },
+  { from: "Las Vegas, NV", to: "Los Angeles, CA" },
+  { from: "Lexington, KY", to: "Louisville, KY" },
+  { from: "Lincoln, NE", to: "Omaha, NE" },
+  { from: "Little Rock, AR", to: "Fayetteville, AR" },
+  { from: "Los Angeles, CA", to: "San Diego, CA" },
+  { from: "Louisville, KY", to: "Lexington, KY" },
+  { from: "Lubbock, TX", to: "Dallas, TX" },
+  { from: "Madison, WI", to: "Milwaukee, WI" },
+  { from: "Memphis, TN", to: "Nashville, TN" },
+  { from: "Miami, FL", to: "Orlando, FL" },
+  { from: "Milwaukee, WI", to: "Chicago, IL" },
+  { from: "Minneapolis, MN", to: "St. Paul, MN" },
+  { from: "Modesto, CA", to: "Fresno, CA" },
+  { from: "Myrtle Beach, SC", to: "Charleston, SC" },
+  { from: "Naples, FL", to: "Miami, FL" },
+  { from: "Nashville, TN", to: "Memphis, TN" },
+  { from: "New Haven, CT", to: "Hartford, CT" },
+  { from: "New Orleans, LA", to: "Baton Rouge, LA" },
+  { from: "New York, NY", to: "Los Angeles, CA" },
+  { from: "North Port, FL", to: "Sarasota, FL" },
+  { from: "Ogden, UT", to: "Salt Lake City, UT" },
+  { from: "Oklahoma City, OK", to: "Tulsa, OK" },
+  { from: "Omaha, NE", to: "Lincoln, NE" },
+  { from: "Orlando, FL", to: "Tampa, FL" },
+  { from: "Oxnard, CA", to: "Los Angeles, CA" },
+  { from: "Palm Bay, FL", to: "Melbourne, FL" },
+  { from: "Panama City, FL", to: "Tallahassee, FL" },
+  { from: "Pensacola, FL", to: "Mobile, AL" },
+  { from: "Peoria, IL", to: "Chicago, IL" },
+  { from: "Philadelphia, PA", to: "New York, NY" },
+  { from: "Phoenix, AZ", to: "Las Vegas, NV" },
+  { from: "Pittsburgh, PA", to: "Cleveland, OH" },
+  { from: "Port St. Lucie, FL", to: "Palm Beach, FL" },
+  { from: "Portland, ME", to: "Boston, MA" },
+  { from: "Portland, OR", to: "Seattle, WA" },
+  { from: "Poughkeepsie, NY", to: "New York, NY" },
+  { from: "Prescott Valley, AZ", to: "Phoenix, AZ" },
+  { from: "Providence, RI", to: "Boston, MA" },
+  { from: "Provo, UT", to: "Salt Lake City, UT" },
+  { from: "Punta Gorda, FL", to: "Cape Coral, FL" },
+  { from: "Raleigh, NC", to: "Durham, NC" },
+  { from: "Reading, PA", to: "Philadelphia, PA" },
+  { from: "Reno, NV", to: "Las Vegas, NV" },
+  { from: "Richmond, VA", to: "Virginia Beach, VA" },
+  { from: "Riverside, CA", to: "Los Angeles, CA" },
+  { from: "Rochester, MN", to: "Minneapolis, MN" },
+  { from: "Rochester, NY", to: "Buffalo, NY" },
+  { from: "Rockford, IL", to: "Chicago, IL" },
+  { from: "Sacramento, CA", to: "San Francisco, CA" },
+  { from: "Salisbury, MD", to: "Baltimore, MD" },
+  { from: "Salt Lake City, UT", to: "Provo, UT" },
+  { from: "San Antonio, TX", to: "Austin, TX" },
+  { from: "San Diego, CA", to: "Los Angeles, CA" },
+  { from: "San Francisco, CA", to: "San Jose, CA" },
+  { from: "San Jose, CA", to: "San Francisco, CA" },
+  { from: "San Luis Obispo, CA", to: "Santa Barbara, CA" },
+  { from: "Santa Maria, CA", to: "Santa Barbara, CA" },
+  { from: "Santa Rosa, CA", to: "San Francisco, CA" },
+  { from: "Savannah, GA", to: "Charleston, SC" },
+  { from: "Scranton, PA", to: "Wilkes-Barre, PA" },
+  { from: "Seattle, WA", to: "Portland, OR" },
+  { from: "Sebastian, FL", to: "Vero Beach, FL" },
+  { from: "Spokane, WA", to: "Seattle, WA" },
+  { from: "Springfield, IL", to: "Chicago, IL" },
+  { from: "Springfield, MA", to: "Boston, MA" },
+  { from: "Springfield, MO", to: "Kansas City, MO" },
+  { from: "St. Louis, MO", to: "Kansas City, MO" },
+  { from: "Stockton, CA", to: "Modesto, CA" },
+  { from: "Tallahassee, FL", to: "Orlando, FL" },
+  { from: "Tampa, FL", to: "Orlando, FL" },
+  { from: "Toledo, OH", to: "Cleveland, OH" },
+  { from: "Trenton, NJ", to: "Philadelphia, PA" },
+  { from: "Tucson, AZ", to: "Phoenix, AZ" },
+  { from: "Tulsa, OK", to: "Oklahoma City, OK" },
+  { from: "United States", to: "Canada" },
+  { from: "Urban Honolulu, HI", to: "Los Angeles, CA" },
+  { from: "Virginia Beach, VA", to: "Norfolk, VA" },
+  { from: "Visalia, CA", to: "Fresno, CA" },
+  { from: "Washington, DC", to: "Baltimore, MD" },
+  { from: "Wilmington, NC", to: "Charlotte, NC" },
+  { from: "Winston, NC", to: "Greensboro, NC" },
+  { from: "Worcester, MA", to: "Boston, MA" },
+  { from: "York, PA", to: "Harrisburg, PA" },
+  { from: "Youngstown, OH", to: "Cleveland, OH" },
+  { from: "New York, NY", to: "Los Angeles, CA" },
+  { from: "Miami, FL", to: "Seattle, WA" },
+  { from: "Chicago, IL", to: "San Francisco, CA" },
+  { from: "Houston, TX", to: "New York, NY" },
+  { from: "Phoenix, AZ", to: "Chicago, IL" },
+  { from: "Dallas, TX", to: "Los Angeles, CA" },
+  { from: "San Diego, CA", to: "New York, NY" },
+  { from: "Atlanta, GA", to: "San Francisco, CA" },
+  { from: "Boston, MA", to: "Los Angeles, CA" },
+  { from: "Philadelphia, PA", to: "Miami, FL" },
+  { from: "Washington, DC", to: "Houston, TX" },
+  { from: "Seattle, WA", to: "Boston, MA" },
+  { from: "Detroit, MI", to: "Los Angeles, CA" },
+  { from: "San Francisco, CA", to: "Miami, FL" },
+  // { from: "Los Angeles, CA", to: "New York, NY" },
+  // { from: "Denver, CO", to: "Miami, FL" },
+  // { from: "Las Vegas, NV", to: "Chicago, IL" },
+  // { from: "San Antonio, TX", to: "San Francisco, CA" },
+  // { from: "Phoenix, AZ", to: "Atlanta, GA" },
+  // { from: "Charlotte, NC", to: "Los Angeles, CA" },
+  // { from: "Cleveland, OH", to: "Seattle, WA" },
+  // { from: "Boston, MA", to: "Atlanta, GA" },
+  // { from: "Chicago, IL", to: "New Orleans, LA" },
+  // { from: "Omaha, NE", to: "New York, NY" },
+  // { from: "San Diego, CA", to: "Houston, TX" },
+  // { from: "Philadelphia, PA", to: "San Jose, CA" },
+  // { from: "Detroit, MI", to: "Miami, FL" },
+  // { from: "New Orleans, LA", to: "Los Angeles, CA" },
+  // { from: "Raleigh, NC", to: "San Francisco, CA" },
+  // { from: "Salt Lake City, UT", to: "Boston, MA" },
+  // { from: "Minneapolis, MN", to: "Miami, FL" },
+  // { from: "Indianapolis, IN", to: "Seattle, WA" },
+  // { from: "Cincinnati, OH", to: "San Diego, CA" },
+  // { from: "Portland, OR", to: "Chicago, IL" },
+  // { from: "Tampa, FL", to: "Las Vegas, NV" },
+  // { from: "Nashville, TN", to: "San Francisco, CA" },
+  // { from: "Birmingham, AL", to: "New York, NY" },
+  // { from: "Buffalo, NY", to: "Los Angeles, CA" },
+  // { from: "Cleveland, OH", to: "Dallas, TX" },
+  // { from: "Jacksonville, FL", to: "Los Angeles, CA" },
+  // { from: "Fort Worth, TX", to: "Seattle, WA" },
+  // { from: "Austin, TX", to: "Los Angeles, CA" },
+  // { from: "Virginia Beach, VA", to: "Miami, FL" },
+  // { from: "Wichita, KS", to: "Los Angeles, CA" },
+  // { from: "Milwaukee, WI", to: "New York, NY" },
+  // { from: "Tucson, AZ", to: "Seattle, WA" }
+];
 
 export default function RealEstateMapComponent() {
   const mountRef = useRef<HTMLDivElement>(null);
@@ -185,7 +388,7 @@ export default function RealEstateMapComponent() {
     price: number;
     position: THREE.Vector3;
   } | null>(null);
-  const [currentYear, setCurrentYear] = useState(2004);
+  const [currentYear, setCurrentYear] = useState(2008);
   const spheresRef = useRef<{ [key: string]: THREE.Mesh }>({});
   const [realEstateData, setRealEstateData] = useState<{
     [key: string]: { [key: string]: number };
@@ -201,13 +404,14 @@ export default function RealEstateMapComponent() {
     }
   };
 
+  const initData = async () => {
+    const data = await fetchData();
+    setRealEstateData(data);
+  };
+
   useEffect(() => {
-    const initData = async () => {
-      const data = await fetchData();
-      setRealEstateData(data);
-    };
     initData();
-  }, []);
+  }, [])
 
   useEffect(() => {
     if (!mountRef.current) return;
@@ -242,17 +446,18 @@ export default function RealEstateMapComponent() {
     controls.enableDamping = true;
 
     // Lighting
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
     scene.add(ambientLight);
 
-    const pointLight = new THREE.PointLight(0xffffff, 1);
+    const pointLight = new THREE.PointLight(0xffffff, 0.5);
     pointLight.position.set(5, 5, 5);
     scene.add(pointLight);
 
     // Create nodes (cities)
-    const minSize = 0.03;
+    const minSize = 0.05;
     const maxSize = 0.15;
 
+    const cityMeshes: { [key: string]: THREE.Mesh } = {};
     Object.entries(cityPositions).forEach(([cityName, position]) => {
       const geometry = new THREE.SphereGeometry(minSize, 32, 32);
       const material = new THREE.MeshStandardMaterial({
@@ -263,6 +468,7 @@ export default function RealEstateMapComponent() {
       const sphere = new THREE.Mesh(geometry, material);
       sphere.position.set(...position);
       scene.add(sphere);
+      cityMeshes[cityName] = sphere;
       spheresRef.current[cityName] = sphere;
 
       // Add label only for specified cities
@@ -279,6 +485,23 @@ export default function RealEstateMapComponent() {
 
       // Add hover effect
       sphere.userData = { name: cityName };
+    });
+
+    // Create migration edges
+    migrations.forEach(({ from, to }) => {
+      const start = cityMeshes[from]?.position;
+      const end = cityMeshes[to]?.position;
+
+      if (start && end) {
+        const points = [];
+        points.push(start.clone());
+        points.push(end.clone());
+
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+        const lineMaterial = new THREE.LineBasicMaterial({ color: 0xd3d3d3, transparent: true, opacity: 0.2 }); // Red color for migration lines
+        const line = new THREE.Line(geometry, lineMaterial);
+        scene.add(line);
+      }
     });
 
     camera.position.z = 3;
@@ -330,7 +553,7 @@ export default function RealEstateMapComponent() {
       mountRef.current?.removeChild(renderer.domElement);
       mountRef.current?.removeChild(labelRenderer.domElement);
     };
-  }, []);
+  }, [currentYear, realEstateData]);
 
   useEffect(() => {
     if (!realEstateData[currentYear.toString()]) return;
@@ -386,7 +609,7 @@ export default function RealEstateMapComponent() {
             </div>
             <div className="mt-4">
               <Slider
-                min={2004}
+                min={2008}
                 max={2023}
                 step={1}
                 value={[currentYear]}
