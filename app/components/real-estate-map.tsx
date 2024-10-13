7220187c32b2c0151e904444bbc0717e7604a0e8
taipioca <leanntai94@gmail.com>
2024-10-13 03:54:56 -0400

    build

--- app/components/real-estate-map.tsx
+++ app/components/real-estate-map.tsx
@@ -9,7 +9,11 @@ interface RealEstateMapProps {
   year: string;
 }
 
-const RealEstateMap: React.FC<RealEstateMapProps> = ({ cityName, cityData, year }) => {
+const RealEstateMap: React.FC<RealEstateMapProps> = ({
+  cityName,
+  cityData,
+  year,
+}) => {
   // Calculate the median price for the given year
   const medianPrice = cityData && year in cityData ? cityData[year] : 0;
 
@@ -18,27 +22,35 @@ const RealEstateMap: React.FC<RealEstateMapProps> = ({ cityName, cityData, year 
   if (cityData && cityData["2022"] && cityData["2023"]) {
     const price2022 = cityData["2022"];
     const price2023 = cityData["2023"];
-    
+
     // Calculate percentage increase
     const increase = price2023 - price2022;
     const percentIncrease = (increase / price2022) * 100;
 
     // Format the market trend as a string with a '+' sign for positive values
-    marketTrend = `${percentIncrease >= 0 ? '+' : ''}${percentIncrease.toFixed(1)}%`;
+    marketTrend = `${percentIncrease >= 0 ? "+" : ""}${percentIncrease.toFixed(
+      1
+    )}%`;
   }
 
   return (
     <Card className="mt-4 text-2xl">
       <CardHeader>
-        <CardTitle>{cityName ? `${cityName} Insights` : "Real Estate Insights"}</CardTitle>
+        <CardTitle>
+          {cityName ? `${cityName} Insights` : "Real Estate Insights"}
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <ul className="space-y-4">
           <li className="flex items-center space-x-2 text-sm">
             <DollarSign className="text-primary" />
             <div>
-              <h3 className="font-semibold">Median Real Estate Price for {year}</h3>
-              <p className="text-2xl text-turquoise">${medianPrice.toLocaleString()}</p>
+              <h3 className="font-semibold">
+                Median Real Estate Price for {year}
+              </h3>
+              <p className="text-2xl text-turquoise">
+                ${medianPrice.toLocaleString()}
+              </p>
             </div>
           </li>
           <li className="flex items-center space-x-2 text-sm">
