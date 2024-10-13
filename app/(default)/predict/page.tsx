3c39a190c8b89dbd7ab497c5f9c00f365f85cd52
josiexw <jxwang4053@gmail.com>
2024-10-12 20:24:50 -0400

    map outline

--- app/(default)/predict/page.tsx
+++ app/(default)/predict/page.tsx
@@ -20,6 +20,7 @@ import { HomeIcon } from "lucide-react";
 import { Button } from "@/app/components/ui/button";
 import { CompareDemo } from "@/app/components/image-slider";
 import { Router } from "next/router";
+import usa from "./usa.png"
 
 // City positions
 const cityPositions: { [key: string]: [number, number, number] } = {
@@ -286,7 +287,7 @@ export default function RealEstateMapComponent() {
           localStorage.getItem("theme") === "dark" ? "white" : "white";
         cityDiv.style.fontSize = "12px";
         const cityLabel = new CSS2DObject(cityDiv);
-        cityLabel.position.set(0, 0.1, 0);
+        cityLabel.position.set(0, 0, 0);
         sphere.add(cityLabel);
       }
 
@@ -311,6 +312,20 @@ export default function RealEstateMapComponent() {
     //   }
     // });
 
+    const textureLoader = new THREE.TextureLoader()
+    textureLoader.load(usa.src, (texture) => {
+      const material = new THREE.MeshBasicMaterial({
+        map: texture,
+        transparent: true,
+        opacity: 0.5,
+        side: THREE.DoubleSide,
+      })
+      const geometry = new THREE.PlaneGeometry(6.8, 4.1)  // Adjust size as needed
+      const usaMap = new THREE.Mesh(geometry, material)
+      usaMap.position.set(0.08, 0.2, -0.1)  // Adjust position as needed
+      scene.add(usaMap)
+    })
+
     camera.position.z = 3;
 
     // Raycaster for hover effect
@@ -328,7 +343,7 @@ export default function RealEstateMapComponent() {
       raycaster.setFromCamera(mouse, camera);
       const intersects = raycaster.intersectObjects(scene.children);
 
-      if (intersects.length > 0 && intersects[0].object instanceof THREE.Mesh) {
+      if (intersects.length > 0 && intersects[0].object instanceof THREE.Mesh && intersects[0].object.userData.name !== undefined) {
         const cityData = intersects[0].object.userData;
         const position = intersects[0].object.position.clone();
         position.project(camera);
